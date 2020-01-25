de9db74de5a978c3b9acbd1e1b0ccbaa6cc9860f
yerx <yer.x.xiong@gmail.com>
2020-01-24 19:05:12 -0500

    service setup

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PhotoCarousel from './components/PhotoCarousel.jsx';
+
+ReactDOM.render(<PhotoCarousel />, document.getElementById('app'));
