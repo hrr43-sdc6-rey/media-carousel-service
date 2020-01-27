c546016605f534e4f462e8e0f85ee2d96cd70785
yerx <yer.x.xiong@gmail.com>
2020-01-27 18:29:16 -0500

    styling images module

--- client/components/MediaDisplay.jsx
+++ client/components/MediaDisplay.jsx
@@ -12,19 +12,36 @@ const SubSection = styled.section`
   grid-template-rows: [row1] 208px [padding] 5px [row2] 208px;
 `;
 
+const Fit100Div = styled.div`
+  object-fit: cover;
+  height: 100%;
+  width: 100%;
+`
+
+// experiment
+const DivGrid = styled.div`
+  grid-area: row1 / col1 / row2 / padding 1;
+`
+
+// experiment
+const ImageFit = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+`
+
+
 const ImageLargePosition1 = styled.img`
-  grid-column: col1 / padding1;
-  grid-row: row1 / row2;
+  grid-area: row1 / col1 / row2 / padding 1;
 `;
 
+
 const ImageLargePosition2 = styled.img`
-  grid-column: col2 / padding2;
-  grid-row: row1 / row2;
+  grid-area: row1 / col2 / row2 / padding 2;
 `;
 
 const ImageSmallPosition1 = styled.img`
-  grid-column: col3 / padding3;
-  grid-row: row1 / padding;
+  grid-area: row1 / col3 / padding / padding 3;
 `;
 
 const ImageSmallPosition2 = styled.img`
@@ -37,6 +54,8 @@ const ImageLargePosition3 = styled.img`
   grid-row: row1 / row2;
 `;
 
+
+
 class MediaDisplay extends React.Component {
   constructor() {
     super();
@@ -46,10 +65,24 @@ class MediaDisplay extends React.Component {
     return (
       <Wrapper>
         <SubSection>
-          <ImageLargePosition1 src="https://picsum.photos/id/1018/312/416" alt="an image of something" />
-          <ImageLargePosition2 src="https://picsum.photos/id/1047/312/416" alt="an image of something" />
+          <Fit100Div>
+            <ImageLargePosition1 src="https://picsum.photos/id/1018/312/416" alt="an image of something" />
+          </Fit100Div>
+          <Fit100Div>
+            <ImageLargePosition2 src="https://picsum.photos/id/1047/312/416" alt="an image of something" />
+          </Fit100Div>
           <ImageSmallPosition1 src="https://picsum.photos/id/1055/156/208" alt="an image of something" />
           <ImageSmallPosition2 src="https://picsum.photos/id/106/156/208" alt="an image of something" />
+          <Fit100Div>
+
+          </Fit100Div>
+          <Fit100Div>
+
+          </Fit100Div>
+          <Fit100Div>
+
+          </Fit100Div>
+
           <ImageLargePosition3 src="https://picsum.photos/id/1069/312/416" alt="an image of something" />
         </SubSection>
       </Wrapper>
