a7f66ea9f8ebeb0f289169da3b82f5d1994085c3
yerx <yer.x.xiong@gmail.com>
2020-02-05 11:13:29 -0500

    in a state that team mates can add repo to their proxy server

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# Media-Carousel-Service
+# Media-And-Info-Service
 
 ## Getting Started
 - Run `npm install`
 - Open two terminal windows. Start up the server with `npm run start:dev` and start up webpack with `npm run build:dev`
 
-## Database
-- Install Mongoose and MongoDB if you don't have it already.
-- Seed the database by running `npm run seed`
+## Getting Started for Adding This Service to Your Proxy Server
+- Git clone the repo
+- In your proxy server's index.html page, add `<div id="photos-and-info"></div>` and `<script src="http://localhost:3000/bundle.js"></script>`
+- cd into Media-Carousel-Service
+- Run `npm install`
+- Run `npm run start:dev`
+- Run `npm run build:dev`
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const ejs = require('ejs');
+
 const app = express();
 const port = process.env.PORT || 3000;
-const db = require('../database/index.js');
 const bodyParser = require('body-parser');
+const db = require('../database/index.js');
 
 app.use(express.static('./public'));
 app.use(bodyParser.json());
@@ -12,23 +13,21 @@ app.engine('html', require('ejs').renderFile);
 
 app.get('/:id', (req, res) => {
   res.render('../public/index.html');
-})
+});
 
 app.get('/api/experiences/:id', (req, res) => {
-  // console.log('id', req.params.id);
-  let experienceId = parseInt(req.params.id);
-  // console.log('type', typeof experienceId);
+  const experienceId = parseInt(req.params.id);
+
   db.getExperiences(experienceId)
     .then((data) => {
       res.status(200).send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).send({
-        message: err.message
+        message: err.message,
       });
     });
 });
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
