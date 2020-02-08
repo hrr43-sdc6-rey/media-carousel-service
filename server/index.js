fab7daf466cbfac7e3171a19de7a120820c92695
yerx <yer.x.xiong@gmail.com>
2020-02-08 16:59:52 -0500

    CORS fix

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
 const db = require('../database/index.js');
 
+app.use(cors());
 app.use(express.static('./public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cors());
 // app.use(cors({
 //   origin: 'http://18.217.113.225:4000',
 //   optionsSuccessStatus: 200,
