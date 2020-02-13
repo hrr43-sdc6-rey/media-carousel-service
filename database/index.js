d42402fa99389e9904f2afa05bb99c13a9d39947
John Balbin <jbalbin3@yahoo.com>
2020-02-12 22:06:58 -0800

    added CRUD routes

--- database/index.js
+++ database/index.js
@@ -6,19 +6,18 @@ mongoose.set('useCreateIndex', true);
 
 const db = mongoose.connection;
 
-let mongooseConnection = 'mongodb://localhost/fec-airbnb';
+const mongooseConnection = 'mongodb://localhost/fec-airbnb';
 
 mongoose
   .connect(mongooseConnection, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Connection error: ', err));
+  .catch((err) => console.error('Connection error: ', err));
 
 const getExperiences = (expId) => {
-  let experienceId = expId;
-
+  const experienceId = expId;
   return new Promise((resolve, reject) => {
     Experience.find({ experienceId })
       .exec((err, data) => {
@@ -30,6 +29,44 @@ const getExperiences = (expId) => {
   });
 };
 
-module.exports = { db, getExperiences };
+const addExperiences = (experience) => {
+  return new Promise((resolve, reject) => {
+    Experience.insert(experience)
+      .exec((err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+  });
+};
 
+const updateExperiences = (expId, experience) => {
+  const experienceId = expId;
+  return new Promise((resolve, reject) => {
+    Experience.replaceOne({ experienceId }, experience)
+      .exec((err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+  });
+};
 
+const deleteExperiences = (expId) => {
+  const experienceId = expId;
+  return new Promise((resolve, reject) => {
+    Experience.deleteOne({ experienceId })
+      .exec((err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+  });
+};
+
+module.exports = {
+  db, getExperiences, addExperiences, updateExperiences, deleteExperiences,
+};
--- server/index.js
+++ server/index.js
@@ -21,6 +21,22 @@ app.get('/:id', (req, res) => {
   res.render('../public/index.html');
 });
 
+// Create
+app.post('/api/experiences/', (req, res) => {
+  // console.log(req.body);
+  // res.status(200).end(JSON.stringify(req.body));
+  db.addExperiences(req.body)
+    .then((data) => {
+      res.status(200).end(data);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: err.message,
+      });
+    });
+});
+
+// Read
 app.get('/api/experiences/:id', (req, res) => {
   const experienceId = parseInt(req.params.id, 10);
 
@@ -35,5 +51,36 @@ app.get('/api/experiences/:id', (req, res) => {
     });
 });
 
+// Update
+app.put('/api/experiences/:id', (req, res) => {
+  const experienceId = parseInt(req.params.id, 10);
+  // console.log(req.body);
+  // res.status(200).end(JSON.stringify(req.body));
+  db.updateExperiences(experienceId, req.body)
+    .then((data) => {
+      res.status(200).end(data);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: err.message,
+      });
+    });
+});
+
+// Delete
+app.delete('/api/experiences/:id', (req, res) => {
+  const experienceId = parseInt(req.params.id, 10);
+  // console.log(req.body);
+  // res.status(200).end(JSON.stringify(req.body));
+  db.deleteExperiences(experienceId)
+    .then((data) => {
+      res.status(200).end(data);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: err.message,
+      });
+    });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
